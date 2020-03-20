0fb06c1e77bccf844a2d629a4d02427114f3bd6c
davide <davide@j8>
2020-03-20 13:16:29 +0100

    fixed stationtype camel case

--- src/main/resources/com/idmsuedtirol/bluetoothtrafficelaboration/stations_bluetooth_link.sql
+++ src/main/resources/com/idmsuedtirol/bluetoothtrafficelaboration/stations_bluetooth_link.sql
@@ -22,6 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
  select id, name as stationcode
    from station
-  where stationtype = 'Linkstation'
+  where stationtype = 'LinkStation'
     and active
   order by id
